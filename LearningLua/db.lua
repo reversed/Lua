44ff1d9e115210e667b7d36e257ff49257e65bfb
reversed <leichen.usst@gmail.com>
2016-11-22 17:09:32 +0800

    add datafiles.lua db.lua

---
+++ LearningLua/db.lua
@@ -0,0 +1,35 @@
+entry{
+      title = "Tecgraf",
+      org = "Computer Graphics Technology Group, PUC-Rio",
+      url = "http://www.tecgraf.puc-rio.br/",
+      contact = "Waldemar Celes",
+      description = [[
+        TeCGraf is the result of a partnership between PUC-Rio,
+        the Pontifical Catholic University of Rio de Janeiro,
+        and <A HREF="http://www.petrobras.com.br/">PETROBRAS</A>,
+        the Brazilian Oil Company.
+        TeCGraf is Lua's birthplace,
+        and the language has been used there since 1993.
+        Currently, more than thirty programmers in TeCGraf use
+        Lua regularly; they have written more than two hundred
+        thousand lines of code, distributed among dozens of
+        final products.]]
+      }
+entry{
+      title = "Tecgraf_02",
+      org = "Computer Graphics Technology Group, PUC-Rio, the 2nd entry",
+      url = "http://www.tecgraf.puc-rio.br/, the 2nd entry",
+      contact = "Waldemar Celes 02",
+      description = [[
+        This is the 2nd entry,
+        TeCGraf is the result of a partnership between PUC-Rio,
+        the Pontifical Catholic University of Rio de Janeiro,
+        and <A HREF="http://www.petrobras.com.br/">PETROBRAS</A>,
+        the Brazilian Oil Company.
+        TeCGraf is Lua's birthplace,
+        and the language has been used there since 1993.
+        Currently, more than thirty programmers in TeCGraf use
+        Lua regularly; they have written more than two hundred
+        thousand lines of code, distributed among dozens of
+        final products.]]
+      }
